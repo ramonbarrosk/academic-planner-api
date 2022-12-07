edb48efa913d9e2d30d66e8140431847372695bd
Ramon Barros <ramonbarros6@gmail.com>
2022-12-07 15:13:57 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create, :destroy]
 
   resources :topics
-  resources :user
+  resources :users
   resources :subjects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
