1d1b37392883c170c7be9be65be177f35aa4a0be
Ramon Barros <ramonbarros6@gmail.com>
2022-12-10 20:04:34 -0300

    wip

--- app/services/authenticate/json_web_token.rb
+++ app/services/authenticate/json_web_token.rb
@@ -2,10 +2,14 @@ class Authenticate::JsonWebToken
   class << self
     def encode(payload, exp = 30.days.from_now)
       payload[:exp] = exp.to_i
+      p "---"
+      p payload
       JWT.encode payload, nil, 'none'
     end
 
     def decode(token)
+      p "---"
+      p token
       decode = JWT.decode token, nil, false
       decode[0]
     rescue
