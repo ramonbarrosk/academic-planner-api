e06cf28d484301cac619b6577365d53014e93f66
Ramon Barros <ramonbarros6@gmail.com>
2022-12-10 18:38:21 -0300

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,11 @@ class ApplicationController < ActionController::API
 
   def authenticate_cookie
     token = Rails.env.development? ? cookies.signed[:"next-auth.session-token"] : get_cookie_prod
-
+    p "---"
+    p token
     decoded_token = Authenticate::JsonWebToken.decode(token)
-
+    p "---"
+    p decoded_token
     if decoded_token && decoded_token["sub"]
       @current_user = User.find_by_id(decoded_token["sub"])
     end
