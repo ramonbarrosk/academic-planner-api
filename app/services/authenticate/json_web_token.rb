a9437528db690816b5cf9660a52df6550b3729d1
Ramon Barros <ramonbarros6@gmail.com>
2022-12-10 20:00:22 -0300

    wip jwt

--- app/services/authenticate/json_web_token.rb
+++ app/services/authenticate/json_web_token.rb
@@ -2,11 +2,12 @@ class Authenticate::JsonWebToken
   class << self
     def encode(payload, exp = 30.days.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base, 'HS256')
+      JWT.encode payload, nil, 'none'
     end
 
     def decode(token)
-      JWT.decode(token, ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base, false, { algorithm: 'HS512' })[0]
+      decode = JWT.decode token, nil, false
+      decode[0]
     rescue
       nil
     end
